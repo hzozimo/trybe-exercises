8df5745e6c71de2295ddac9e8ff16aefb542e50e
hzozimo <hzozimo@gmail.com>
2021-02-24 19:43:49 -0300

    programa maior de tres adicionado

---
+++ bloco_4/dia_1/maior_de_tres.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 3;
+let c = 5;
+
+if ((a > b) && (a > c))
+    console.log("O primeiro número é maior")
+else
+    if ((b > a) && (b > c))
+        console.log("O segundo número é maior")
+    else
+        if ((c > a) && (c > b))
+            console.log("O terceiro número é maior")
