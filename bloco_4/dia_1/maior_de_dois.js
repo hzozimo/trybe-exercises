946ee05afe052330a10bef7fe9410e7e8c9ff312
hzozimo <hzozimo@gmail.com>
2021-02-24 19:27:32 -0300

    programa maior de dois adicionado

---
+++ bloco_4/dia_1/maior_de_dois.js
@@ -0,0 +1,10 @@
+let a = 2;
+let b = 1;
+
+if (a > b) 
+    console.log("o primeiro número é maior") 
+    else
+        if (b > a) 
+        console.log("O segundo número é maior") 
+        else 
+        console.log("os dois npumeros são iguais")
