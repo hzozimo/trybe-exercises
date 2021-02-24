4fe862cc1090b1d7e33d36bef238c3738e3c6161
hzozimo <hzozimo@gmail.com>
2021-02-24 16:40:53 -0300

    exercicios de fixaçao feitos

---
+++ Fixação/4.1/scripts.js
@@ -0,0 +1,42 @@
+// //Exercício de fixação da parte II da aula 4.1
+// const name = "Henrique";
+// const birthCity = "Teresópolis";
+// let birthYear = 1984;
+
+// console.log(name, birthCity, birthYear)
+
+// birthYear = 2020;
+
+// console.log(birthYear)
+
+// birthCity = "Rio de Janeiro";
+
+// console.log(birthCity)
+
+//exercício de fixação da parte III da aula 4.1
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId)
+// console.log(typeof patientAge)
+
+// patientId = '50';
+
+// console.log(typeof patientId)
+
+let base = 5;
+let altura = 8;
+
+let area = base * altura;
+
+console.log(area)
+
+let perimetro = base + base + altura + altura;
+
+console.log(perimetro)
