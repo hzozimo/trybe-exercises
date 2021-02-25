3ed49ced06e31c85a95769cdc4e57ab496c9955b
hzozimo <hzozimo@gmail.com>
2021-02-25 16:28:07 -0300

    exercicios 3 e 4 feitos

--- bloco_4/dia_2/exercicios.js
+++ bloco_4/dia_2/exercicios.js
@@ -15,3 +15,19 @@ for (let i = 0; i < numbers.length; i++) {
     soma = soma + numbers[i]
 }
 console.log("A soma é " + soma)
+
+//exercicio 3
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let mediaAritimetica = soma / numbers.length;
+console.log("A média entre os valores é " + mediaAritimetica)
+
+//exercicio 4
+//Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (mediaAritimetica > 20) {
+    console.log("valor maior que 20")
+}
+else {
+    console.log("valor menor ou igual a 20")
+}
