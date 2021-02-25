e2de040b403e945efd45be97b5dd8ed0ddee8aa1
hzozimo <hzozimo@gmail.com>
2021-02-25 14:41:55 -0300

    programas par e impar criados

---
+++ bloco_4/dia_1/impar.js
@@ -0,0 +1,6 @@
+let a = 2;
+let b = 4;
+let c = 6;
+impar = ((a % 2 == 1) || (b % 2 == 1) || (c % 2 == 1))
+
+console.log(impar)
---
+++ bloco_4/dia_1/par.js
@@ -0,0 +1,6 @@
+let a = 3;
+let b = 5;
+let c = 7;
+par = ((a % 2 == 0) || (b % 2 == 0) || (c % 2 == 0))
+
+console.log(par)
