4541df6428c5ca95ea901f3d2832ba541d0721b2
hzozimo <hzozimo@gmail.com>
2021-02-25 14:03:41 -0300

    programa notas criado

---
+++ bloco_4/dia_1/notas.js
@@ -0,0 +1,18 @@
+let nota = 10;
+
+if (nota >=90) 
+    console.log("Conceito A")
+else
+    if (nota >=80)
+        console.log("Conceito B")
+    else
+        if (nota >=70)
+            console.log("Conceito C")
+        else
+            if (nota >=60)
+                console.log("COnceito D")
+            else
+                if (nota >= 50)
+                    console.log("Conceito E")
+                else
+                    console.log("Conceito F")
