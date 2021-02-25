bc1df25445e4a8ee8f4caa50a968a6f851817b30
hzozimo <hzozimo@gmail.com>
2021-02-25 19:19:54 -0300

    exercicios bonus 1, 2 e 3 feitos

--- bloco_4/dia_2/exercicios.js
+++ bloco_4/dia_2/exercicios.js
@@ -85,3 +85,48 @@ for (index = 0; index < 25; index++) {
     divisao.push((numbers2[index] / 2))
 }
 console.log(`${divisao}`)
+
+//exercicio bônus 1
+//Ordene o array numbers em ordem crescente e imprima seus valores;
+
+for (let index = 1; index < numbers.length; index++) {
+    for (let index2 = 0; index2 < index; index2++) {
+      if (numbers[index] < numbers[index2]) {
+        let reserva = numbers[index];
+  
+        numbers[index] = numbers[index2];
+        numbers[index2] = reserva;
+      }
+    }
+  }
+console.log(`O array ordenado de forma crescente é: ${numbers}`)
+
+//exerccicio bonus 2
+//Ordene o array numbers em ordem decrescente e imprima seus valores;
+for (let index = 1; index < numbers.length; index++) {
+    for (let index2 = 0; index2 < index; index2++) {
+      if (numbers[index] > numbers[index2]) {
+        let reserva = numbers[index2];
+  
+        numbers[index2] = numbers[index];
+        numbers[index] = reserva;
+      }
+    }
+  }
+console.log(`O array ordenado de forma decrescente é: ${numbers}`)
+
+//exercicio bonus 3
+//Agora você irá criar um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo: 
+// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let multiplicado = []
+
+for (let index = 0; index < numbers.length -1; index++) {
+          multiplicado.push((numbers[index]*numbers[index +1]))
+  }
+multiplicado.push((numbers[(numbers.length-1)]*2))
+
+console.log(numbers.length)
+console.log(`O array multiplicado é: ${multiplicado}`) 
+
