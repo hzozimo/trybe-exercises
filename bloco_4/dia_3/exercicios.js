3b8bb5f35e29adee02184e5706a512bb63342030
hzozimo <hzozimo@gmail.com>
2021-02-26 19:32:40 -0300

    exercicio 6 feito

--- bloco_4/dia_3/exercicios.js
+++ bloco_4/dia_3/exercicios.js
@@ -125,3 +125,22 @@ console.log(linha)
 esquerda = esquerda -1;
 direita = direita +1;
 }
+
+//exercicio bonus 6
+//Faça um programa que diz se um número definido numa variável é primo ou não.
+//Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+//Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+let numero = 12;
+let contador = 0;
+for (let index =1; index <= numero; index++){
+    if (numero%index == 0){
+        contador++
+    }
+}
+
+if (contador <= 2){
+    console.log(`O número ${numero} é primo`)
+} else {
+    console.log(`O número ${numero} não é primo e possui ${contador} divisores`)
+}
