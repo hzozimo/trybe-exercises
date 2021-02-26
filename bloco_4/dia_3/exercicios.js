b61200ac5989f2a25507ceb9f754447767db4036
hzozimo <hzozimo@gmail.com>
2021-02-26 15:26:41 -0300

    exercicio 2 feito

--- bloco_4/dia_3/exercicios.js
+++ bloco_4/dia_3/exercicios.js
@@ -21,3 +21,25 @@ for (let index = 0; index < n; index++) {
     console.log(linha)
 }
 
+//exercicio 2
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
+n = 5;
+
+for (let index = 0; index < n; index++) {
+    let linha = ""
+    for (let index2 = 0; index2 <=index; index2++) {
+        //linha.push("*")
+        linha = linha + "*"
+    }
+    console.log(linha)
+}
+
+
