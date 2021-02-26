dfe43e6571162d056f109cbf4792fda17e210f10
hzozimo <hzozimo@gmail.com>
2021-02-26 15:52:36 -0300

    exercicio 3 feito

--- bloco_4/dia_3/exercicios.js
+++ bloco_4/dia_3/exercicios.js
@@ -14,8 +14,7 @@ let n = 5;
 
 for (let index = 0; index < n; index++) {
     let linha = ""
-    for (let index2 = 0; index2 <n; index2++) {
-        //linha.push("*")
+    for (let index2 = 0; index2 < n; index2++) {
         linha = linha + "*"
     }
     console.log(linha)
@@ -36,10 +35,31 @@ n = 5;
 for (let index = 0; index < n; index++) {
     let linha = ""
     for (let index2 = 0; index2 <=index; index2++) {
-        //linha.push("*")
         linha = linha + "*"
     }
     console.log(linha)
 }
 
 
+//exercício 3
+// Agora inverta o lado do triângulo. Por exemplo:
+// n = 5
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+//Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+n = 5;
+for (let index = 1; index <= n; index++) {
+    let linha = ""
+    for (let index2 = 1; index2 <=n; index2++) {
+        if (index + index2 <= n){
+            linha = linha +" "
+        } else {
+        linha = linha + "*"
+        }
+    }
+    console.log(linha)
+}
