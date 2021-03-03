ec4e4b87968b896467c7398d5eecc1635834c1c1
hzozimo <hzozimo@gmail.com>
2021-03-03 19:01:12 -0300

    exercicios de fixaçao 5.1 terminados

---
+++ Fixação/5.1/script.js
@@ -0,0 +1,8 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink';
+document.querySelectorAll('h3')[0].style.backgroundColor = 'BlueViolet';
+document.querySelectorAll('h3')[1].style.backgroundColor = 'BlueViolet';
+document.querySelectorAll('h3')[2].style.backgroundColor = 'black';
+document.querySelectorAll('h3')[3].style.backgroundColor = 'black';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'yellow';
+document.getElementById('footer-container').style.backgroundColor = 'DarkSlateGray'
