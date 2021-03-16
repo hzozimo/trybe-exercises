676b849fe3dcb819ea2bb97f6a470203702e98f9
hzozimo <hzozimo@gmail.com>
2021-03-16 18:16:49 -0300

    commit inicial

---
+++ bloco_6/dia_2/script.js
@@ -0,0 +1,12 @@
+function adicionaEstados() {
+    let estado = ['', 'Acre (AC)', 'Alagoas (AL)', 'Amapá (AP)', 'Amazonas (AM)', 'Bahia (BA)', 'Ceará (CE)', 'Distrito Federal (DF)', 'Espírito Santo (ES)',
+    'Goiás (GO)', 'Maranhão (MA)', 'Mato Grosso (MT)', 'Mato Grosso do Sul (MS)', 'Minas Gerais (MG)', 'Pará (PA)', 'Paraíba (PB)', 'Paraná (PR)',
+    'Pernambuco (PE)', 'Piauí (PI)', 'Rio de Janeiro (RJ)', 'Rio Grande do Norte (RN)', 'Rio Grande do Sul (RS)', 'Rondônia (RO)', 'Roraima (RR)',
+    'Santa Catarina (SC)', 'São Paulo (SP)', 'Sergipe (SE)', 'Tocantins (TO)'];
+    for (index of estado) {
+        let opcao = document.createElement('option');
+        let estados = document.getElementById('estado');
+        estados.appendChild(opcao).innerText = index;
+    }
+}
+adicionaEstados();
