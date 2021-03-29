ed6e3d8149712a5a1a3939e163225be433b23a32
hzozimo <hzozimo@gmail.com>
2021-03-29 18:22:24 -0300

    exercicio 1 feito

---
+++ bloco_8/dia_1/exercicio_I.js
@@ -0,0 +1,17 @@
+const retornaObjeto = (nome) => ({
+    nomeCompleto: `${nome}`,
+    email:`${nome.replace(/ /g, '_')}@trybe.com`
+});
+
+const newEmployees = () => {
+    const employees = {
+      id1: retornaObjeto('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+      id2: retornaObjeto('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+      id3: retornaObjeto('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+    }
+    return employees;
+  };
+
+const employees = newEmployees();
+
+console.table(employees);
