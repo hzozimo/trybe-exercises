bc117c5766deef6245b163894bb592f98b4239fe
hzozimo <hzozimo@gmail.com>
2021-03-30 13:54:12 -0300

    implementado o forEach com ajuda do Isaac

--- bloco_8/dia_1/exercicio_IV.js
+++ bloco_8/dia_1/exercicio_IV.js
@@ -3,9 +3,10 @@ const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const compara = (gabarito, respostas , funcao) => {
     let total = 0;
-for (let index = 0; index < rightAnswers.length; index += 1){
-    total += funcao(gabarito[index], respostas[index]);
-}
+// for (let index = 0; index < gabarito.length; index += 1){
+//     total += funcao(gabarito[index], respostas[index]);
+// }
+gabarito.forEach((el, index) =>{ total += funcao(el, respostas[index])});
 return total;
 }
 
